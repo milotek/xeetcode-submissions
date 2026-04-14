a3d616e8fc51eb83b5d32b1fa52268e50ecb51c0
milo <milo@milotek.dev>
2026-04-14 15:29:41 +0100

    Add: number-of-one-bits - submission-7

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        if n == 0: return 0
+        halves = 1
+        while n > 2:
+            if n % 2 != 0:
+                n = n - 1
+                halves += 1
+            n = n / 2
+        return halves
+
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         if n == 0: return 0
+#         return n % 2 + self.hammingWeight(n >> 1)
